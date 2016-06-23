99e59b53bd4fa4c09d6f5e21ecce10637ef446b3
ricardo04 <remartinezc1@hotmail.com>
2016-06-23 12:25:17 -0600

    la parte del responsibe y de los empleados en procesos

--- controllers/php/employeeController.php
+++ controllers/php/employeeController.php
@@ -1,5 +1,10 @@
 <?php
     App::getHead('Employees');
+?>
+
+    <div id="main-container" class="container-fluid row full">
+
+<?php
     App::getLeftMain();
     App::getRightMain();
     App::getEmployee();
