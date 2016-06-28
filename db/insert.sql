3856a2d45b21650b33e6f7c69416b5f75c8fd21e
Eleazar Martinez <eleazarg2112@gmail.com>
2016-06-28 11:07:18 -0600

    haciendo dinamicas las insidencias

--- db/insert.sql
+++ db/insert.sql
@@ -5,4 +5,4 @@ insert into empleado(nombre1,nombre2,apellido1,apellido2,cedula,fecha_ingreso,id
 values('westly','alejandro','meza','sotomayor','001-160695-0026D','2000-2-2',1,1,1);
 /*password : ebco123*/
 insert into usuario(correo,password,id_empleado,foto,role,estado)
-values('west@correo.com','a1d5285401e5441cf7ff053c4276c1af764f4ef3',1,'120161106.jpg',1,1);
+values('westlymeza@gmail.com','a1d5285401e5441cf7ff053c4276c1af764f4ef3',1,'120161106.jpg',1,1);
