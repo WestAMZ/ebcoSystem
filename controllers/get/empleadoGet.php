d30f83233c1356219e199ca867c82e438dd9ba92
Agente <Agente>
2016-06-29 12:17:12 +0200

    modificacion empleado

--- controllers/get/empleadoGet.php
+++ controllers/get/empleadoGet.php
@@ -6,9 +6,10 @@
         if(isset($_GET['id']))
         {
             $id = $_GET['id'];
-            Connection::connet();
-            $query = "";
-            $empleado = result ->feth_assoc();
+            Connection::connect();
+            $query = "SELECT u.id_empleado, `nombre1`, `nombre2`, `apellido1`, `apellido2`, `cedula`, `telefono`, `firma`, `id_puesto`, `id_sitio`, `id_jefe`, `inss`, `fecha_ingreso`, e.estado, `correo` FROM empleado e INNER JOIN usuario u WHERE u.id_empleado = e.id_empleado AND u.id_empleado='$id'";
+            $result = Connection::getConnection()->query($query);
+            $empleado = $result ->fetch_assoc();
             echo ('{ "empleado" : [' );
             echo(JSON_encode($empleado));
             echo (']}' );
