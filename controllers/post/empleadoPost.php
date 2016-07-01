3c0fce21d2635e8c44c12086b6c0f94a35e905e6
WestAMZ <westly15@hotmail.com>
2016-07-01 08:05:02 -0600

    avance empleado

--- controllers/post/empleadoPost.php
+++ controllers/post/empleadoPost.php
@@ -14,15 +14,18 @@
             }
             else
             {
+
                 echo ($empleado->add_error);
             }
         }
         //mode 1 : update
         else if($_GET['mod']==1)
         {
+            //firma esta en null
             $id_mod = $_GET['id'];
-            $empleado = new Empleado ($id_mod,$_POST['nombre1'],$_POST['nombre2'],$_POST['apellido1'],$_POST['apellido2'],$_POST['cedula'],$_POST['telefono'],$_POST['firma'],$_POST['id_puesto'],$_POST['id_sitio'],$_POST['id_jefe'],$_POST['inss'],null,1);
+            $empleado = new Empleado ($id_mod,$_POST['nombre1'],$_POST['nombre2'],$_POST['apellido1'],$_POST['apellido2'],$_POST['cedula'],$_POST['telefono'],null,$_POST['id_puesto'],$_POST['id_sitio'],$_POST['id_jefe'],$_POST['inss'],null,1);
             $empleado->update();
+            echo(1);
         }
     }
 ?>
